3c2c4a73b946fdeb1c28b6ed38d0c0709ea31d08
matheus-tatibano-de-campos <matheustatibano@gmail.com>
2024-12-13 10:49:41 -0300

    first commit

---
+++ realtime/urls.py
@@ -0,0 +1,10 @@
+from xml.etree.ElementInclude import include
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('chat.urls')),
+    path('admin/', admin.site.urls),
+
+]
